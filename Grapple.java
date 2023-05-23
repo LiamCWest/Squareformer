fa3521647a1577eb8db427b840ad58a4313493d0
LiamCWest <764394@pdsb.net>
2023-05-23 19:43:01 -0400

    raycast hit workingg

--- Grapple.java
+++ Grapple.java
@@ -1,13 +1,14 @@
 import java.awt.*;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Grapple {
     private GameObject holder;
     private GameManager gameManager;
     private Point mousePos;
     private int[] grapplePos = new int[2];
-    private Line2D line = new Line2D.Double();
+    Line2D grappleLine = new Line2D.Double();
 
     public Grapple(GameObject holder, GameManager gameManager){
         this.holder = holder;
@@ -20,7 +21,8 @@ public class Grapple {
         Object[] hit = raycast(grapplePos, angle);
         GameObject hitObject = (GameObject)hit[0];
         double[] hitPoint = (double[])hit[1];
-        System.out.println(hitObject != null ? hitObject : "null");
+        grappleLine.setLine(grapplePos[0], grapplePos[1], hitPoint[0], hitPoint[1]);
+        System.out.println(hit != null ? hitObject + " " + hitPoint[0] + " " + hitPoint[1] : "null");
     }
 
     public void draw(Graphics2D g){
@@ -34,7 +36,7 @@ public class Grapple {
         int xMod = (int)(Math.cos(angle)*length);
         int yMod = (int)(Math.sin(angle)*length);
         
-        g.draw(line);
+        g.draw(grappleLine);
         g.drawLine(grapplePos[0], grapplePos[1], grapplePos[0]+xMod, grapplePos[1]+yMod);
         g.setStroke(stroke);
     }
@@ -48,75 +50,51 @@ public class Grapple {
         return Math.atan2(mousePos.getY()-grapplePos[1], mousePos.getX()-grapplePos[0]);
     }
 
-    // raycast method taking in a point and a direction as an angle
-    public Object[] raycast(int[] point, double angle){
-        // create a new point with the angle and a large distance
-        int[] newPoint = new int[]{(int)(point[0]+Math.cos(angle)*1000), (int)(point[1]+Math.sin(angle)*1000)};
-        // create a new line with the point and the new point
-        line = new Line2D.Double(point[0], point[1], newPoint[0], newPoint[1]);
-        // list of hit objects
-        ArrayList<GameObject> hitObjects = new ArrayList<GameObject>();
-        // loop through all the game objects
-        for(GameObject gameObject : gameManager.getGameObjects()){
-            // if the game object is not the holder and the game object is a shape
-            if(gameObject != holder && gameObject.isShapeQ()){
-                if(line.intersects(gameObject.getHitbox())) {
-                    hitObjects.add(gameObject);
-                }
-            }
-        }
-        GameObject closestObject = null;
-        double[] closestPoint = null;
+    public Object[] raycast (int[] point, double angle){
+        ArrayList<Map<GameObject, double[]>> hitObjects = new ArrayList<Map<GameObject, double[]>>();
 
-        // if there are hit objects
-        if(!(hitObjects.isEmpty())){
-            // get the closest hit object
-            for(GameObject gameObject : hitObjects){
-                for(Polygon shape : gameObject.getShapes()){
-                    for(int i = 0; i < shape.npoints; i++){
-                        double[][] shapeLine = new double[][]{{shape.xpoints[i]+gameObject.getX(), shape.ypoints[i]+ gameObject.getY()}, {shape.xpoints[(i+1)%shape.npoints] + gameObject.getX(), shape.ypoints[(i+1)%shape.npoints] + gameObject.getY()}};
-                        double[] intersection = calculateIntersection(shapeLine, new double[][]{{point[0], point[1]}, {newPoint[0], newPoint[1]}});
-                        if(intersection != null){
-                            if(closestPoint == null || Math.sqrt(Math.pow(intersection[0]-point[0], 2)+Math.pow(intersection[1]-point[1], 2)) < Math.sqrt(Math.pow(closestPoint[0]-point[0], 2)+Math.pow(closestPoint[1]-point[1], 2))){
-                                closestPoint = intersection;
-                                closestObject = gameObject;
-                                line = new Line2D.Double(point[0], point[1], intersection[0], intersection[1]);
-                                System.out.println("new" + " " + line.getX2() + " " + line.getY2());
-                            }
-                        }
+        double xDir = Math.cos(angle);
+        double yDir = Math.sin(angle);
+
+        for(GameObject object : gameManager.getGameObjects()){
+            if(object != holder){
+                double x = point[0];
+                double y = point[1];
+                boolean hit = false;
+
+                for (int i = 0; i < 1000; i++){
+                    if (!hitObject(x, y, object.getHitbox())){
+                        x += xDir;
+                        y += yDir;
+                    }else {
+                        hit = true;
+                        break;
                     }
                 }
+
+                if (hit)hitObjects.add(Map.of(object, new double[]{x, y}));
             }
-            System.out.println("final" + " " + line.getX2() + " " + line.getY2());
-            // return the closest hit object
-            return new Object[]{closestObject, closestPoint};
         }
-        // return null if no game object is found
-        return null;
+
+        if (hitObjects.size() > 0){
+            double[] closestHit = hitObjects.get(0).values().iterator().next();
+            GameObject closestObject = hitObjects.get(0).keySet().iterator().next();
+
+            for (Map<GameObject, double[]> hitObject : hitObjects){
+                double[] hitPoint = hitObject.values().iterator().next();
+                GameObject object = hitObject.keySet().iterator().next();
+
+                if (Math.sqrt(Math.pow(point[0]-hitPoint[0], 2)+Math.pow(point[1]-hitPoint[1], 2)) < Math.sqrt(Math.pow(point[0]-closestHit[0], 2)+Math.pow(point[1]-closestHit[1], 2))){
+                    closestHit = hitPoint;
+                    closestObject = object;
+                }
+            }
+
+            return new Object[]{closestObject, closestHit};
+        }else return new Object[]{null, null};
     }
 
-    public static double[] calculateIntersection(double[][] line1, double[][] line2) {
-        double x1 = line1[0][0];
-        double y1 = line1[0][1];
-        double x2 = line1[1][0];
-        double y2 = line1[1][1];
-    
-        double x3 = line2[0][0];
-        double y3 = line2[0][1];
-        double x4 = line2[1][0];
-        double y4 = line2[1][1];
-    
-        double denominator = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4));
-    
-        if (denominator == 0) {
-            // Lines are parallel, no intersection point
-            return null;
-        }
-    
-        double intersectionX = (((x1 * y2) - (y1 * x2)) * (x3 - x4) - (x1 - x2) * ((x3 * y4) - (y3 * x4))) / denominator;
-        double intersectionY = (((x1 * y2) - (y1 * x2)) * (y3 - y4) - (y1 - y2) * ((x3 * y4) - (y3 * x4))) / denominator;
-    
-        return new double[]{intersectionX, intersectionY};
+    private boolean hitObject(double x, double y, Rectangle hitbox){
+        return x >= hitbox.getX() && x <= hitbox.getX()+hitbox.getWidth() && y >= hitbox.getY() && y <= hitbox.getY()+hitbox.getHeight();
     }
-    
 }
